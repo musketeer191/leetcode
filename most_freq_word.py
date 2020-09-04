ba6285176a9a13d537ed2d7437ef7e45345cf726
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-09-04 11:33:02 -0700

    working solution for most frequent word challenge, except 1 edge case

---
+++ most_freq_word.py
@@ -0,0 +1,40 @@
+import collections
+from string import punctuation
+from bisect import bisect_left
+
+
+class Solution:
+    def is_in_banned(self, w, banned):
+        # given a word and a sorted list of banned words, return if the word is in banned words
+        idx = bisect_left(banned, w)
+        if (idx < len(banned)) and (banned[idx] == w):
+            return True
+        return False
+
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        # need to count occurence of words not in banned list
+        # as we not care about punctuation, should strip them away
+        # not case sensitive, so just convert all to lower cases
+        # simple sol: split the paragraph into words,
+        # loop thru the list to count occurence of words:
+        # if meet a word not in banned, increment its count
+
+        # complexity: for each word need to search for it in banned list, so log(len(banned)) if banned is sorted, then look up its count to increase, which is O(1) if the counts are stored in a hashmap. So final complexity is len(words) * log(len(banned))
+
+        # lower case
+        exclude = set(punctuation)
+        s = paragraph.lower()
+        # strip punctuations & split into words
+        # edge case: the string has words delimited by comma, or some punctuation
+        # todo: handle edge case via re.split()
+        s = ''.join([ch for ch in s if ch not in exclude])
+        words = s.split(' ')
+
+        banned = sorted(banned)
+        count = collections.Counter()
+        for w in words:
+            if not self.is_in_banned(w, banned):
+                count[w] += 1
+
+        return count.most_common(1)[0][0]
+
