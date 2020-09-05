dfa5a4f9752cd9ddebeb6929dd44608c0956ca22
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-09-04 19:34:37 -0700

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,6 +1,15 @@
-import os
-import pandas as pd
-import numpy as np
-
 d = {'a': 1, 'b': 2}
 sorted(d.items(), key=lambda x: x[1])
+
+
+def find_last_occur(v, ls):
+    # last occur is the first occur in reverse order
+    try:
+        return len(ls) - 1 - ls[::-1].index(v)
+    except ValueError:
+        return -1
+
+
+if __name__ == '__main__':
+    ls = [4, 5, 6, 4, 5]
+    print(find_last_occur(4, ls))
